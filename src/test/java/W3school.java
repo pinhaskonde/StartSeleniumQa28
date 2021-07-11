d93ac36292acfc19d1ce7056a059ed23a4290c30
pinhaskonde <53976223+pinhaskonde@users.noreply.github.com>
2021-07-11 17:02:11 +0300

    add homework (css.selector)

--- src/test/java/W3school.java
+++ src/test/java/W3school.java
@@ -1,9 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class W3school {
@@ -15,24 +18,34 @@ public class W3school {
         wd = new ChromeDriver();
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.navigate().to("");
+        wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
     }
 
     @Test
     public  void w3schoolTest(){
-        //numbers of rows
+        List<WebElement>elements = wd.findElements(By.cssSelector("tr"));                           //numbers of rows
+        System.out.println("Numbers of rows in the table: " + elements.size());
 
-        //numbers of columns
+        List<WebElement>columns = wd.findElements(By.cssSelector("tr th"));                         //numbers of columns
+        System.out.println("Numbers of columns in the table: " + columns.size());
 
-        //print rows 3
+        String row3 = wd.findElement(By.cssSelector("tbody tr:nth-child(4)")).getText();            //print row 3
+        System.out.println("Row #3: " + row3);
 
-        //print lastColumns
+        List<WebElement> lastColumns = wd.findElements(By.cssSelector("tbody td:last-child"));      //print lastColumns
+        int colNumb = 0;
+        for (WebElement element :lastColumns) {
+            colNumb++;
+            System.out.println("Last column in the row #" + colNumb + ": " + element.getText());
+        }
 
-        //print Maria Anders
+        String str = wd.findElement(By.cssSelector("tbody td:nth-child(2)")).getText();             //print Maria Anders
+        System.out.println("M.A.: "+str);
+
     }
 
     @AfterMethod
     public void postCondition(){
-
+        wd.quit();
     }
 }
